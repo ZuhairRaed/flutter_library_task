cdb4f5197f2a081fe9ad2d33e9e06a3df9229e46
OsamaAssaf <osama.assaf.y@gmail.com>
2023-02-12 12:34:10 +0300

    Osama Assaf Commit

--- lib/views/technical_form_view.dart
+++ lib/views/technical_form_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_library_task/widgets/text_field_title.dart';
 
 class TechnicalFormView extends StatelessWidget {
-   TechnicalFormView({super.key});
+  TechnicalFormView({super.key});
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
@@ -19,9 +19,12 @@ class TechnicalFormView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Technical Support'),
-        leading: IconButton(onPressed: (){
-          Navigator.pop(context);
-        }, icon: const Icon(Icons.arrow_back_ios),),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
       ),
       body: Form(
         key: _formKey,
@@ -31,156 +34,120 @@ class TechnicalFormView extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-
                 const TextFieldTitle(title: 'Name'),
                 const SizedBox(height: 8.0),
                 TextFormField(
-                  decoration:  InputDecoration(
+                  decoration: InputDecoration(
                     hintText: 'Please enter your name here',
-                    hintStyle:const TextStyle(
+                    hintStyle: const TextStyle(
                       fontSize: 14.0,
                       fontWeight: FontWeight.w500,
                       color: Color(0xffB7B7B7),
                     ),
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(4.0),
-                      borderSide:const BorderSide(
-                        color: Color(0xFF757575)
-                      )
-                    ),
+                        borderRadius: BorderRadius.circular(4.0),
+                        borderSide: const BorderSide(color: Color(0xFF757575))),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(4.0),
-                        borderSide:const BorderSide(
-                            color: Color(0xFF757575)
-                        )
-                    ),
+                        borderSide: const BorderSide(color: Color(0xFF757575))),
                     focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(4.0),
-                        borderSide:const BorderSide(
-                            color: Color(0xFF757575)
-                        )
-                    ),
+                        borderSide: const BorderSide(color: Color(0xFF757575))),
                   ),
                 ),
                 const SizedBox(height: 16.0),
-
                 const TextFieldTitle(title: 'Email'),
                 const SizedBox(height: 8.0),
                 TextFormField(
-                  decoration:  InputDecoration(
+                  decoration: InputDecoration(
                     hintText: 'Please enter your email here',
-                    hintStyle:const TextStyle(
+                    hintStyle: const TextStyle(
                       fontSize: 14.0,
                       fontWeight: FontWeight.w500,
                       color: Color(0xffB7B7B7),
                     ),
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(4.0),
-                        borderSide:const BorderSide(
-                            color: Color(0xFF757575)
-                        )
-                    ),
+                        borderSide: const BorderSide(color: Color(0xFF757575))),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(4.0),
-                        borderSide:const BorderSide(
-                            color: Color(0xFF757575)
-                        )
-                    ),
+                        borderSide: const BorderSide(color: Color(0xFF757575))),
                     focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(4.0),
-                        borderSide:const BorderSide(
-                            color: Color(0xFF757575)
-                        )
-                    ),
+                        borderSide: const BorderSide(color: Color(0xFF757575))),
                   ),
                 ),
                 const SizedBox(height: 16.0),
-
                 const TextFieldTitle(title: 'Issue Topic'),
                 const SizedBox(height: 8.0),
                 DropdownButtonFormField(
-                  decoration:  InputDecoration(
+                  decoration: InputDecoration(
                     hintText: 'Please enter your email here',
-                    hintStyle:const TextStyle(
+                    hintStyle: const TextStyle(
                       fontSize: 14.0,
                       fontWeight: FontWeight.w500,
                       color: Color(0xffB7B7B7),
                     ),
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(4.0),
-                        borderSide:const BorderSide(
-                            color: Color(0xFF757575)
-                        )
-                    ),
+                        borderSide: const BorderSide(color: Color(0xFF757575))),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(4.0),
-                        borderSide:const BorderSide(
-                            color: Color(0xFF757575)
-                        )
-                    ),
+                        borderSide: const BorderSide(color: Color(0xFF757575))),
                     focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(4.0),
-                        borderSide:const BorderSide(
-                            color: Color(0xFF757575)
-                        )
-                    ),
-                  ), items: issueItems.map((item) {
+                        borderSide: const BorderSide(color: Color(0xFF757575))),
+                  ),
+                  items: issueItems.map((item) {
                     return DropdownMenuItem(
                       value: item,
-                      onTap: (){},
+                      onTap: () {},
                       child: Text(item),
                     );
-                }).toList(), onChanged: (String? value) {  },
+                  }).toList(),
+                  onChanged: (String? value) {},
                 ),
                 const SizedBox(height: 16.0),
-
-
                 const SizedBox(height: 16),
                 const TextFieldTitle(title: 'Issue Description'),
                 TextFormField(
                   minLines: 5,
-                    maxLines: 10,
-                    decoration:  InputDecoration(
-                      hintText: 'Please enter your email here',
-                      hintStyle:const TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xffB7B7B7),
-                      ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(4.0),
-                          borderSide:const BorderSide(
-                              color: Color(0xFF757575)
-                          )
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(4.0),
-                          borderSide:const BorderSide(
-                              color: Color(0xFF757575)
-                          )
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(4.0),
-                          borderSide:const BorderSide(
-                              color: Color(0xFF757575)
-                          )
-                      ),
+                  maxLines: 10,
+                  decoration: InputDecoration(
+                    hintText: 'Please enter your email here',
+                    hintStyle: const TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xffB7B7B7),
                     ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(4.0),
+                        borderSide: const BorderSide(color: Color(0xFF757575))),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(4.0),
+                        borderSide: const BorderSide(color: Color(0xFF757575))),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(4.0),
+                        borderSide: const BorderSide(color: Color(0xFF757575))),
+                  ),
                 ),
                 const SizedBox(height: 64),
-
-                Center(child: FilledButton(
-                  style: FilledButton.styleFrom(
-                    fixedSize: const Size(188.0, 45.0),
-                    backgroundColor: const Color(0xFF1F2F49),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5)
-                    )
+                Center(
+                  child: FilledButton(
+                    style: FilledButton.styleFrom(
+                        fixedSize: const Size(188.0, 45.0),
+                        backgroundColor: const Color(0xFF1F2F49),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5))),
+                    onPressed: () {},
+                    child: const Text(
+                      'Submit',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
                   ),
-                  onPressed: (){}, child: const Text('Submit',style: TextStyle(
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.w500,
-                ),),),
                 ),
               ],
             ),
